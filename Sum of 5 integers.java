27bd368ae0f3dc69f5550feb1f1f9b042533ab44
Kathi-Swaraj <139244069+Kathi-Swaraj@users.noreply.github.com>
2023-07-11 19:38:54 +0530

    Sum of 5 integers.java

---
+++ Sum of 5 integers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        
+        int sum = a+b+c+d+e;
+        System.out.println("The sum of 5 integers is:"+sum);
+        
+    }
+}
